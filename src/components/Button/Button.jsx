26e60954c1b3966d2c48eb3dffc05d10baada863
Mykokola <119622278+Mykokola@users.noreply.github.com>
2023-07-25 18:29:58 +0000

    Deploying to gh-pages from @ Mykokola/goit-react-hw-04-images@0c1c89e26789d12ea7e1add0d2b1d79e689f8c4c 🚀

--- src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import { Button } from './Button.styled';
-import PropTypes from 'prop-types'
-export const ButtonLoadMore = ({ loadMore }) => {
-  return <Button onClick={loadMore}>Load more</Button>;
-};
-ButtonLoadMore.propTypes = {
-    loadMore:PropTypes.func.isRequired
-}
--- src/components/FetchImg.jsx
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-export const fetchImgj = async (imagesName,numb=1) => {
-    const response = await axios.get(`https://pixabay.com/api/?q=${imagesName}&page=${numb}&key=38205688-1a64b11afc73d687830058e8a&image_type=photo&orientation=horizontal&per_page=12`)
-    return response
-}
